0bdd0a70093833b2edf8260645b4d1dad963accd
dadashef30 <millermliesstone@gmail.com>
2025-05-16 02:26:30 +0100

    Create Home.js

---
+++ src/client/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/App.css';
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to Karrotify!</h2>
+      <p>A platform for [describe purpose, e.g., local sharing].</p>
+    </div>
+  );
+}
+
+export default Home;
