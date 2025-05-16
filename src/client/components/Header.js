3c1147f94a29276b165c5b9d98e8ec42e5e7c052
dadashef30 <millermliesstone@gmail.com>
2025-05-16 02:25:41 +0100

    Create Header.js

---
+++ src/client/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/App.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>Karrotify</h1>
+      <nav>
+        <a href="/">Home</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
