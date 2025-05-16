ec7bcd6bce681403917a16d90028da19a8e63282
dadashef30 <millermliesstone@gmail.com>
2025-05-16 02:29:18 +0100

    Create User.js

---
+++ src/server/models/User.js
@@ -0,0 +1,9 @@
+// Example user model (e.g., for Mongoose)
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+});
+
+module.exports = mongoose.model('User', userSchema);
